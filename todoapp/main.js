de9e07749e7defbc1727752dec8ea47320177445
Niko <monika.kan@hotmail.com>
2022-01-30 18:46:50 +0900

    Makes undo work
    With a bug, it restores all deleted items

--- todoapp/main.js
+++ todoapp/main.js
@@ -52,8 +52,10 @@ function showTasks() {
     //activating delete all button
     if(listArr.length > 0) {
         deleteAllBtn.classList.add("active");
+        undoBtn.classList.add("active");
     } else {
         deleteAllBtn.classList.remove("active");
+        undoBtn.classList.remove("active");
     }
 
     let newLiTag = "";
@@ -66,10 +68,30 @@ function showTasks() {
     inputBox.value = "";
 }
 
+
+//storing deleted tasks
+function storageDel(listArr,index) {
+   // let userData = inputBox.value;
+    let getLocalStorageForUndo = localStorage.getItem("New Deleted");
+
+    if(getLocalStorageForUndo == null) {
+        listDeleted = [];
+    } else {
+        //JSON string => js object
+        listDeleted = JSON.parse(getLocalStorageForUndo);
+    }
+    listDeleted.push(listArr[index]);
+    //js object => JSON string
+    localStorage.setItem("New Deleted", JSON.stringify(listDeleted));
+
+}
+
 //deleting tasks
 function deleteTask(index) {
    let getLocalStorage = localStorage.getItem("New Todo");
    listArr = JSON.parse(getLocalStorage);
+    //storing deleted tasks
+    storageDel(listArr,index);
    //deleting a particular li index
    listArr.splice(index, 1);
    //updating the list
@@ -86,3 +108,34 @@ deleteAllBtn.onclick = () => {
     localStorage.setItem("New Todo", JSON.stringify(listArr));
     showTasks();
 }
+
+//undoing
+undoBtn.onclick = () => {
+    let getLocalStorageForUndo = localStorage.getItem("New Deleted");
+    if(getLocalStorageForUndo == null) {
+        listDeleted = [];
+    } else {
+        //JSON string => js object
+        listDeleted = JSON.parse(getLocalStorageForUndo);
+    }
+    
+    
+    let newLiTag = "";
+    listDeleted.forEach((element, index) => {
+        newLiTag += `<li>${element}<span onclick = "deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+    });
+    //adding new li tag inside ul tag
+    todoList.innerHTML = newLiTag;
+
+    localStorage.setItem("New Todo", JSON.stringify(listDeleted));
+    showTasks();
+}
+    
+
+
+// undoBtn.onclick = () => {
+//     listArr.splice(index,0,removed.shift());
+
+//     localStorage.setItem("New Todo", JSON.stringify(listArr));
+//     showTasks();
+// }
